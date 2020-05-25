4c4f50e681e420133b34e56769edf77e955a8760
ankitdobhal <dobhal.ankit@protonmail.com>
2020-05-25 21:34:37 +0530

    Ashok v1.1

--- Ashok.py
+++ Ashok.py
@@ -11,24 +11,36 @@ from core.linkextractor import extract
 from core.bannergrab import banner
 from core.subdomains import sub
 from core.geoip import geo
-from plugins.whoislookup import whois
+from core.wayback import waybackurl,waybackrobots,waybackjson
+from core.gdork import dork
 from plugins.dnslookup import dnslookup
 from plugins.subnetlookup import subnetlookup
 from plugins.httpheaders import httpheader
 from plugins.techanalyzer import techno
-os.system('tput setaf 3')
+os.system('tput setaf 9')
 print("""
-   ___         __         __  
-  / _ |  ___  / /  ___   / /__
- / __ | (_-< / _ \/ _ \ /  '_/
-/_/ |_|/___//_//_/\___//_/\_\   v1.0 Osint/Recon Swiss Knife
-                                > by ankitdobhal
-                              
+
+
+ ▄▄▄        ██████  ██░ ██  ▒█████   ██ ▄█▀
+▒████▄    ▒██    ▒ ▓██░ ██▒▒██▒  ██▒ ██▄█▒ 
+▒██  ▀█▄  ░ ▓██▄   ▒██▀▀██░▒██░  ██▒▓███▄░ 
+░██▄▄▄▄██   ▒   ██▒░▓█ ░██ ▒██   ██░▓██ █▄ 
+ ▓█   ▓██▒▒██████▒▒░▓█▒░██▓░ ████▓▒░▒██▒ █▄
+ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░ ▒ ░░▒░▒░ ▒░▒░▒░ ▒ ▒▒ ▓▒
+  ▒   ▒▒ ░░ ░▒  ░ ░ ▒ ░▒░ ░  ░ ▒ ▒░ ░ ░▒ ▒░
+  ░   ▒   ░  ░  ░   ░  ░░ ░░ ░ ░ ▒  ░ ░░ ░  > Recon Swiss Army Knife
+      ░  ░      ░   ░  ░  ░    ░ ░  ░  ░      v1.1   
 """)
-os.system('tput setaf 2')
+os.system('tput setaf 6')
+print("""
+ Author  : Ankit Dobhal | Break||The||Code
+ Github  : https://github.com/ankitdobhal
+ Website : http://ankitdobhal.github.io/ 
+ =========================================
+	""")
+os.system('tput setaf 3')
 parser = argparse.ArgumentParser()
 parser.add_argument('--headers',help='Httpheaders of target url')
-parser.add_argument('--whois',help='Whois of target domain')
 parser.add_argument('--dns',help='Dnslookup of target domain')
 parser.add_argument('--subdomain',help='Subdomain lookup of target domain')
 parser.add_argument('--nmap',help='Nmapscan of target domain')
@@ -38,18 +50,14 @@ parser.add_argument('--extract',help='Extract links from target url(https/http)'
 parser.add_argument('--cidr',help='Cidr for subnetlookup of target')
 parser.add_argument('--banner',help='Banner grabing of target ip address')
 parser.add_argument('--geoip',help='GeoIP lookup of target ip address')
+parser.add_argument('--wayback',help="Internet Archive Crawling of target domain")
+parser.add_argument('--dorknumber',help="Google dorking results number")
 args = parser.parse_args()
 
 if args.headers:
 	print("[+] Extracing http headers of target url")
 	os.system('tput setaf 10')
 	httpheader(args.headers)
-	exit()
-
-if args.whois:
-	print("[+] Whois lookup of target domain")
-	os.system('tput setaf 7')
-	whois(args.whois)
 	exit()
 
 if args.nmap:
@@ -102,4 +110,15 @@ if args.geoip:
 	print("[+] Geoip lookup of target Ip address")
 	os.system('tput setaf 10')
 	geo(args.geoip)
+	exit()
+
+if args.wayback:
+	print("[+] Dumping and Crawling Internet Archive Machine With Ashok")
+	waybackurl(args.wayback)
+	waybackrobots(args.wayback)
+	waybackjson(args.wayback)
+	exit()
+
+if args.dorknumber:
+	dork(args.dorknumber)
 	exit()
